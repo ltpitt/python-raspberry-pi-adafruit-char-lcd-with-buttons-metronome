e59db3b88335d878a644be94f9276807d47a89a6
Davide Nastri <d.nastri@gmail.com>
2024-10-20 18:20:10 +0200

    feat: Improved metronome precision at high speeds

--- metronome.py
+++ metronome.py
@@ -3,19 +3,27 @@ import simpleaudio as sa
 import configparser
 import os
 import time
+import threading
+
 
 def set_system_volume(volume):
-    # Convert the volume to a percentage (0-100%)
     volume_percentage = max(0, min(int(volume * 100), 100))
     print(volume_percentage)
     os.system(f'amixer sset \'PCM\' {volume_percentage}%')
+
+
+def play_tick(sound_path):
+    while True:
+        wave_obj = sa.WaveObject.from_wave_file(sound_path)
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
+
 
 def run_metronome():
     config = configparser.ConfigParser()
     config_file_path = os.path.join(os.path.dirname(__file__), 'config.cfg')
     config.read(config_file_path)
-
-    bmp = config.getint('metronome', 'bmp')
+    bpm = config.getint('metronome', 'bpm')
     is_metronome_enabled = config.getboolean('metronome', 'is_metronome_enabled')
     audio_file = config.get('metronome', 'audio_file')
     volume = config.getfloat('metronome', 'volume')
@@ -25,16 +33,16 @@ def run_metronome():
 
     sound_path = os.path.join(os.path.dirname(__file__), 'samples', audio_file)
     print(sound_path)
-    wave_obj = sa.WaveObject.from_wave_file(sound_path)
 
+    delay = 60 / bpm
     while True:
         if is_metronome_enabled:
-            delay = 60 / bmp
-            play_obj = wave_obj.play()
-            play_obj.wait_done()  # Wait until the sound has finished playing
-            time.sleep(delay)  # No need to adjust for the sound's duration
+            print(f"Ticking at {bpm} BPM")
+            threading.Thread(target=play_tick, args=(sound_path,)).start()
+            time.sleep(delay)
         else:
-            time.sleep(1)  # Sleep for one second if metronome is disabled
+            time.sleep(1)
+
 
 if __name__ == "__main__":
     with daemon.DaemonContext():
